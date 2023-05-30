dd5dfa4adef6a3432e4e139db35a58ef5f36f253
Isaque <isaque.teixeira@g.globo>
2023-05-30 16:12:14 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerRJ_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerRJ_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerRJ_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C8B19ED5-2A80-4F1C-AEDE-F0E573827C90")]
+[assembly: AssemblyVersion("1.0.0.0")]
